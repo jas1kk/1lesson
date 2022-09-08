6fe201833fdd9e9a4b8d479252b761c5df83f30d
jas1kk <109805550+jas1kk@users.noreply.github.com>
2022-09-08 23:20:53 +0600

    Add files via upload

---
+++ 5 lesson/script.js
@@ -0,0 +1,54 @@
+//1
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let max = arr[0];
+
+// arr.forEach(function (elem, index) {
+
+//     if(index > 0 ){
+//         if(max < elem){
+//             max = elem;
+//         }
+
+//     }
+// });
+
+// console.log(max);
+
+
+// let min = arr[0];
+
+// arr.forEach(function (elem, index) {
+
+//     if(index > 0 ){
+//         if(min < elem){
+//             mmin = elem;
+//         }
+
+//     }
+// });
+
+//3
+
+// const M = 3, N = 4;
+ 
+// var arr = [];
+// for (var i = 0; i < M; i++) {
+//      arr[i] = [12];
+// }
+ 
+// console.log(arr);
+
+// console.log(min);
+
+//4
+
+// let arr = [22, 94, 63, 56, 142, 17, 117, 25, 345, 792, 966];
+
+// for (let i = 0; i < arr.length; i++)
+// {
+//   if ((arr[i] % 2) === 0)
+//   {
+//   	console.log(arr[i]);
+//   }
+// }
