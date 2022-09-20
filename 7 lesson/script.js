d09d694689d46b5ed4a132e8afd731cbf14819d6
jas1kk <109805550+jas1kk@users.noreply.github.com>
2022-09-20 17:02:52 +0600

    Add files via upload

---
+++ 7 lesson/script.js
@@ -0,0 +1,25 @@
+//1
+
+// function get_current_age(date) {
+
+// return ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
+
+// }
+
+ 
+
+// alert(get_current_age('2005-08-02'));
+
+
+
+
+//3
+
+// const setBg = () => {
+//     const randomColor = Math.floor(Math.random()*16777215).toString(16);
+//     document.body.style.backgroundColor = "#" + randomColor;
+//     color.innerHTML = "#" + randomColor;
+//   }
+  
+//   genNew.addEventListener("click", setBg);
+//   setBg();
